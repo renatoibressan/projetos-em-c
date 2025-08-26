214435d9a5b8a51e3ccaadda7a21e42dd57d5e6a
Renato Bressan <renatoikeda98@gmail.com>
2025-08-26 12:46:02 -0300

    Update calc3.c

--- calc3.c
+++ calc3.c
@@ -2,7 +2,7 @@
 
 int main() {
     int option = 0;
-    while (option != 1 && option == 0) {
+    do {
         printf("Renato Bressan Calculator | Version 3.0\n");
         float num1, num2;
         char operador;
@@ -13,64 +13,87 @@ int main() {
             case '+':
                 resultado = num1 + num2;
                 printf("Result: %.2f\n", resultado);
-                printf("Enter '0' to continue or '1' to exit: \n");
+                printf("Enter '0' to continue or any other number to exit: \n");
                 scanf("%d", &option);
-                if (option == 1) {
+                if (option != 0) {
                     printf("Thanks for using the calculator!\n");
-                    return 0;
+                    break;
+                }
+                else if (option == 0) {
+                    printf("Proceeding to the calculator...\n");
+                    break;
                 }
                 break;
             case '-':
                 resultado = num1 - num2;
                 printf("Result: %.2f\n", resultado);
-                printf("Enter '0' to continue or '1' to exit: \n");
+                printf("Enter '0' to continue or any other number to exit: \n");
                 scanf("%d", &option);
-                if (option == 1) {
+                if (option != 0) {
                     printf("Thanks for using the calculator!\n");
-                    return 0;
+                    break;
+                }
+                else if (option == 0) {
+                    printf("Proceeding to the calculator...\n");
+                    break;
                 }
                 break;
             case '*':
                 resultado = num1 * num2;
                 printf("Result: %.2f\n", resultado);
-                printf("Enter '0' to continue or '1' to exit: \n");
+                printf("Enter '0' to continue or any other number to exit: \n");
                 scanf("%d", &option);
-                if (option == 1) {
+                if (option != 0) {
                     printf("Thanks for using the calculator!\n");
-                    return 0;
+                    break;
+                }
+                else if (option == 0) {
+                    printf("Proceeding to the calculator...\n");
+                    break;
                 }
                 break;
             case '/':
                 resultado = num1 / num2;
                 if (num2 == 0) {
                     printf("No numbers can be divided by 0\n");
-                    printf("Enter '0' to continue or '1' to exit: \n");
+                    printf("Enter '0' to continue or any other number to exit: \n");
                     scanf("%d", &option);
-                    if (option == 1) {
+                    if (option != 0) {
                         printf("Thanks for using the calculator!\n");
-                        return 0;
+                        break;
+                    }
+                    else if (option == 0) {
+                        printf("Proceeding to the calculator...\n");
+                        break;
                     }
                 } else {
                     printf("Result: %.2f\n", resultado);
-                    printf("Enter '0' to continue or '1' to exit: \n");
+                    printf("Enter '0' to continue or any other number to exit: \n");
                     scanf("%d", &option);
-                    if (option == 1) {
+                    if (option != 0) {
                         printf("Thanks for using the calculator!\n");
-                        return 0;
+                        break;
+                    }
+                    else if (option == 0) {
+                        printf("Proceeding to the calculator...\n");
+                        break;
                     }
                 }
                 break;
             default:
                 printf("Invalid operator\n");
-                printf("Enter '0' to continue or '1' to exit: \n");
+                printf("Enter '0' to continue or any other number to exit: \n");
                 scanf("%d", &option);
-                if (option == 1) {
+                if (option != 0) {
                     printf("Thanks for using the calculator!\n");
-                    return 0;
+                    break;
                 }
-                break;
+                else if (option == 0) {
+                    printf("Proceeding to the calculator...\n");
+                    break;
+                }
         }
-    }
+    } while (option == 0);
     return 0;
 
 }
